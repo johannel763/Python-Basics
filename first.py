20ed1b574644ae4710833d1cc7f8fa3bcd98ae70
johannel763 <84607125+johannel763@users.noreply.github.com>
2024-06-07 12:01:10 +1000

    String Slicing

---
+++ first.py
@@ -0,0 +1,25 @@
+msg = "Hello World!"
+
+print(msg[0:3])
+
+print(msg[:])
+
+print(msg[0:-1])
+
+print(msg[-1:])
+
+full_name = "Hendrik Johannes Nel"
+
+middle_name = full_name[8:16]
+
+first_name = full_name[0:7]
+
+last_name = full_name[17:]
+
+print(middle_name)
+
+print(first_name)
+
+print(last_name)
+
+print(full_name[0: len(full_name)])
